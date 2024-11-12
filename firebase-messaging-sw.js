fd6408d65a124fb59080411c03e41e6ba2c11b35
paulinasolorzano <32572042+paulinasolorzano@users.noreply.github.com>
2024-11-12 10:04:53 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 
-//importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-//importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // Configuración de Firebase
